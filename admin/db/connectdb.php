a05e59842b8c07d43ba4da1d0d223b5a435bd7f5
Aurore Bartel <a.bartel@comsea.fr>
2023-05-22 09:34:54 +0200

    bdd online

--- admin/db/connectdb.php
+++ admin/db/connectdb.php
@@ -1,12 +1,12 @@
 <?php
-try {
+/*try {
     $dbh = new PDO('mysql:host=127.0.0.1;dbname=ancre;charset=utf8;port=8889', 'root', 'Comsea', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
 } 
-/*
+*/
 try {
     $dbh = new PDO('mysql:host=globalcbdd.mysql.db;dbname=globalcbdd;charset=utf8', 'globalcbdd', 'Globalaxe08', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
-}*/
+}
